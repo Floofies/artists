a12949949269ff5b48422e18cd571693c6d63efc
Dani <fantasticdragons@gmail.com>
2019-05-20 20:18:36 -0400

    Size canvas to initial window size

--- Lin.js
+++ Lin.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function () {
 	const canvas = document.querySelector("#glcanvas");
+	canvas.height = document.height;
+	canvas.widfth = document.width;
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
 	function rand(maxLength = 0, maxInt = 9) {
